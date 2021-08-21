2c99c6518ed6484d01052980be07e06d77e2215f
Mounica <mounica@pop-os.localdomain>
2021-08-21 15:34:41 +0530

    added homepage

---
+++ src/homepage.component.jsx
@@ -0,0 +1,47 @@
+import react from "react";
+import './homepage.styles.scss';
+
+const HomePage = () => {
+  <div className="homepage">
+    <div className="directory-menu">
+
+      <div className="menu-item">
+        <div className="contnet">
+          <h1 className="title">HATS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+
+      <div className="menu-item">
+        <div className="contnet">
+          <h1 className="title">JACKETS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+
+      <div className="menu-item">
+        <div className="contnet">
+          <h1 className="title">WOMEN'S</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+
+      <div className="menu-item">
+        <div className="contnet">
+          <h1 className="title">MEN'S</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+
+      <div className="menu-item">
+        <div className="contnet">
+          <h1 className="title">HATS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+
+    </div>
+  </div>
+};
+
+export default HomePage; 
