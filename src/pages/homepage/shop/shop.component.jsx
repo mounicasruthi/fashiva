30e03c6b11d3df0cd9bc85640e95e68a60e7cf9f
Mounica <mounica@pop-os.localdomain>
2021-09-09 02:16:44 +0530

    added collection preview and collection item components

--- src/pages/homepage/shop/shop.component.jsx
+++ src/pages/homepage/shop/shop.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import SHOP_DATA from './shop.date';
+import CollectionPreview from '../../../components/collection-preview/collection-preview.component';
 
 class ShopPage extends React.Component {
     constructor(props) {
@@ -13,7 +14,14 @@ class ShopPage extends React.Component {
 
     render()
     {
-        return <div>SHOP PAGE</div>
+        const {collections} = this.state;
+        return ( <div className='shop-page'>
+            {
+                collections.map(({id, ...otherCollectionProps}) => (
+                    <CollectionPreview key={id} {...otherCollectionProps} />
+                ))
+            }
+        </div>);
     }     
 
 }
