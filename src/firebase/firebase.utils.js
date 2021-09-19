19fbec9f52881df32263523c51c0809fb11efd00
Mounica <mounica@pop-os.localdomain>
2021-09-20 00:24:25 +0530

    implementing sign-in feature

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,17 +1,41 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
-import 'firebase/compat/auth';
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
 
 const config = {
-    apiKey: "AIzaSyCokiRiqRpeHyAUFpZW5bxgaNGqZtDchTs",
-    authDomain: "fashiva-db.firebaseapp.com",
-    projectId: "fashiva-db",
-    storageBucket: "fashiva-db.appspot.com",
-    messagingSenderId: "99058813635",
-    appId: "1:99058813635:web:c1866e64ff7d19141a3076",
-    measurementId: "G-W36HM6GE0N"
-  };
+  apiKey: "AIzaSyCokiRiqRpeHyAUFpZW5bxgaNGqZtDchTs",
+  authDomain: "fashiva-db.firebaseapp.com",
+  projectId: "fashiva-db",
+  storageBucket: "fashiva-db.appspot.com",
+  messagingSenderId: "99058813635",
+  appId: "1:99058813635:web:c1866e64ff7d19141a3076",
+  measurementId: "G-W36HM6GE0N",
+};
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
+};
 
 firebase.initializeApp(config);
 
@@ -19,34 +43,7 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
-
-// import { initializeApp } from 'firebase/app';
-// import { getFirestore } from 'firebase/firestore/lite';
-// import { getAuth, signInWithPopup,  GoogleAuthProvider  } from "firebase/auth";
-
-
-// const config = {
-//     apiKey: "AIzaSyCokiRiqRpeHyAUFpZW5bxgaNGqZtDchTs",
-//     authDomain: "fashiva-db.firebaseapp.com",
-//     projectId: "fashiva-db",
-//     storageBucket: "fashiva-db.appspot.com",
-//     messagingSenderId: "99058813635",
-//     appId: "1:99058813635:web:c1866e64ff7d19141a3076",
-//     measurementId: "G-W36HM6GE0N"
-//   };
-
-
-//   const app = initializeApp(config);
-
-//   export const auth = getAuth();
-//   export const firestore = getFirestore();
-  
-//   const provider = new GoogleAuthProvider();
-//   provider.setCustomParameters({ prompt: 'select_account' });
-//   export const signInWithGoogle = () => signInWithPopup(provider);
-  
-//   export default app;
