24fdbb68aab7420e9c36997502bf7548c5fceebf
Mounica <mounica@pop-os.localdomain>
2021-09-19 22:58:54 +0530

    integrated firebase into application

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React from 'react';
 
 import './custom-button.styles.scss';
 
-const CustomButton = ({children, ...otherProps}) => (
-    <button className='custom-button'>
-        {children}
-    </button>
-)
+const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
+  <button
+    className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
+    {...otherProps}
+  >
+    {children}
+  </button>
+);
+
 export default CustomButton;
+
+
+
+// import React from "react";
+
+// import './custom-button.styles.scss';
+
+// const CustomButton = ({children, ...otherProps}) => (
+//     <button className='custom-button'>
+//         {children}
+//     </button>
+// )
+// export default CustomButton;
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -19,7 +19,34 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-  provider.setCustomParameters({promt: 'select_account'});
-  export const signInWithGoogle = () => auth.signInWithGoogle(provider);
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
-  export default firebase;
+export default firebase;
+
+// import { initializeApp } from 'firebase/app';
+// import { getFirestore } from 'firebase/firestore/lite';
+// import { getAuth, signInWithPopup,  GoogleAuthProvider  } from "firebase/auth";
+
+
+// const config = {
+//     apiKey: "AIzaSyCokiRiqRpeHyAUFpZW5bxgaNGqZtDchTs",
+//     authDomain: "fashiva-db.firebaseapp.com",
+//     projectId: "fashiva-db",
+//     storageBucket: "fashiva-db.appspot.com",
+//     messagingSenderId: "99058813635",
+//     appId: "1:99058813635:web:c1866e64ff7d19141a3076",
+//     measurementId: "G-W36HM6GE0N"
+//   };
+
+
+//   const app = initializeApp(config);
+
+//   export const auth = getAuth();
+//   export const firestore = getFirestore();
+  
+//   const provider = new GoogleAuthProvider();
+//   provider.setCustomParameters({ prompt: 'select_account' });
+//   export const signInWithGoogle = () => signInWithPopup(provider);
+  
+//   export default app;
