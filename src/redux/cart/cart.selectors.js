91f11d0009026425de56b06a505c1cff3b1e6a82
Mounica <mounica@pop-os.localdomain>
2021-10-08 15:16:15 +0530

    updated app with reselect and replaced card selectors with memoized selectors

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -1,16 +1,18 @@
-import { createSelector } from "reselect";
+import { createSelector } from 'reselect';
 
-const selectCart = (state) => state.cart;
+const selectCart = state => state.cart;
 
 export const selectCartItems = createSelector(
   [selectCart],
-  (cart) => cart.cartItems
+  cart => cart.cartItems
 );
 
 export const selectCartItemsCount = createSelector(
   [selectCartItems],
-  cartItems.reduce(
-    (accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity,
-    0
-  )
+  cartItems =>
+    cartItems.reduce(
+      (accumalatedQuantity, cartItem) =>
+        accumalatedQuantity + cartItem.quantity,
+      0
+    )
 );
