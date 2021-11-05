b551ac29d953a3f87bc5f7b08834c2526bcf266f
Mounica <mounica@pop-os.localdomain>
2021-11-05 16:19:54 +0530

    integrated stripe payments

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51JsA5gSCY3eZtFk1f8kn1i2TsGAa5AS62qSnz5N9NSk6IDnJNOAHeL33A62lfDwQXdI815Z0aLO7FYZsfCDLrcwU008Z566wlf";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Fashiva"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
